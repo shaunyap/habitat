ae57b4cb3168acb76646dc4f23b3a77ab28c8bbf
Scott Macfarlane <smacfarlane@chef.io>
2019-11-05 09:16:22 -0800

    Move cargo update from schedule task to finish_release pipeline
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- .expeditor/scripts/finish_release/cargo_update.sh
+++ .expeditor/scripts/finish_release/cargo_update.sh
@@ -13,6 +13,13 @@ toolchain="$(get_toolchain)"
 install_rustup
 install_rust_toolchain "$toolchain"
 
+echo "--- :ruby: Install hub"
+gem install hub
+
+echo "--- Print git config "
+git config user.name
+git config user.email
+
 echo "--- :box: Cargo Update"
 cargo +"$toolchain" update
 echo "--- :box: Cargo Check"
@@ -20,11 +27,11 @@ cargo +"$toolchain" check --quiet --all --tests
 
 git add Cargo.lock
 
-
 git commit -s -m "Update Cargo.lock"
 
 # https://expeditor.chef.io/docs/reference/script/#open-pull-request
-open_pull_request
+echo "--- :github: Open Pull Request"
+#hub pull-request --no-edit 
 
 git checkout master 
 git branch -D "$branch"
